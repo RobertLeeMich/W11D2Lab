a3d42ed544785363971230ea7e960779e30894f7
Robert Michaud <guildwarsguy123@yahoo.com>
2023-08-29 11:46:24 -0400

    Tip calculator express application created.

--- server.js
+++ server.js
@@ -2,32 +2,39 @@ const express = require('express')
 
 const app = express()
 
-app.get("/", function (req, res) { 
-    res.send("<h1 style='color:red' >Hello Express!</h1>")
+// app.get("/", function (req, res) { 
+//     res.send("<h1 style='color:red' >Hello Express!</h1>")
+// })
+
+//GREETING ROUTE
+app.get ("/greeting/:name", function(request, response){
+    response.send(`<h1>Hello, ${request.params.name}</h1>`)
 })
 
-app.get ("/greeting/:name", function(req, res){
-    res.send("<h1>Hello,</h1>" + req.params.name)
+////////////////////////////////////////////////
+
+app.get("/tip/:total/:tip", (total, tipPercentage) => {
+    tipPercentage.send(`${parseInt(total.params.total) * (parseInt(total.params.tip) / 100)}`)
 })
 
 
 
 
-app.get("/home", function(req, res){
-    // res.send("<h1>Home Page</h1>") //prints text
-    // res.redirect("/") //redirects to /
-    // res.json({ //sends us to a page with json on it when we go to /home
-    //     name: "katie",
-    //     isCool: true
-    // })
-
-
-})
-
-app.get("/item/:id", (req, res) => {
-    // res.send("This route's id parameter" + req.params.id)
-})
 
 app.listen(3000, function() {
     console.log("Listening on port 3000")
 })
+
+// app.get("/home", function(req, res){
+//     // res.send("<h1>Home Page</h1>") //prints text
+//     // res.redirect("/") //redirects to /
+//     // res.json({ //sends us to a page with json on it when we go to /home
+//     //     name: "katie",
+//     //     isCool: true
+//     // })
+// })
+
+// app.get("/item/:id", (req, res) => {
+//     // res.send("This route's id parameter" + req.params.id)
+// })
+
