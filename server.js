e045105b80cf64946bc19e5c1837ac2c40d6c8b1
Robert Michaud <guildwarsguy123@yahoo.com>
2023-08-29 11:46:49 -0400

    Magic 8 ball express application created

--- server.js
+++ server.js
@@ -18,6 +18,13 @@ app.get("/tip/:total/:tip", (total, tipPercentage) => {
 })
 
 
+const quotes = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+
+
+app.get("/magic/:quote", (request, response) => {
+    const randomElement = Math.floor(Math.random() * quotes.length)
+    response.send(`<h1>${request.params.quote}</h1> <h1>${quotes[randomElement]}</h1>`)
+})
 
 
 
